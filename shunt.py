256e2c9fa03ec18e92915eb0f2269a73255b6d3f
Darren Regan <darren111894@gmail.com>
2019-03-27 13:39:00 +0000

    If c == ')' code
    If c == ')', create a while loop where stack[-1] !='('
    
    Which means you get the last char in the string and loop through poping off stack until you reach and open bracket

--- shunt.py
+++ shunt.py
@@ -10,12 +10,18 @@ def sYard(infix):
   postfix = ""
   stack = ""
 
-
   for c in infix: #
     if c == '(':
-
+      stack = stack + c #Push ( to the stack
     elif c == ')':
-
+      while stack[-1] != '(': #-1 = last char in string stack
+        #Keep finding and poping off the stack until you find the open bracket
+        postfix = postfix + stack[-1]
+        stack = stack[:-1] #Up to but not including last bracket
+      stack = stack[:-1] #Removes open bracket
     elif c in specials: #Is c in special Dictionary
+
+    else:
+      postfix = postfix + c
 
   return postfix
